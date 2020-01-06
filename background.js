e93d1f90a5e66e352ce69550516ea714ffa96bec
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-01-06 23:47:10 +0530

    Basic Setup

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.browserAction.onClicked.addListener(buttonClicked)
+
+function buttonClicked(tab){
+	console.log("button clicked");
+	let msg={
+		txt:"hello"
+	}
+	chrome.tabs.sendMessage(tab.id,msg);
+}
